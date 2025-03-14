a4baa3e6dd6980fbe4d59f6f4b127e6a59335a41
VKyl <viktor.kyliukh@gmail.com>
2025-03-15 00:03:41 +0200

    defined AComplex

--- VKyl_OOP_Lab02/AComplex.h
+++ VKyl_OOP_Lab02/AComplex.h
@@ -1,18 +1,36 @@
 #pragma once
+class TComplex;
 class AComplex
 {
-private: 
+private:
 	double _re, _im;
 public:
-	AComplex(double re, double im): _re(re), _im(im) {}
+	AComplex(double re, double im) : _re(re), _im(im) {}
 	AComplex(const AComplex& c) = default;
 	AComplex(AComplex&& c) = default;
+	AComplex(const TComplex&& c);
+
 	~AComplex() = default;
 
 	double& re() { return _re; }
-	double re() const{ return _re; }
+	double re() const { return _re; }
 
 	double& im() { return _im; }
 	double im() const { return _im; }
+
+	AComplex& operator=(AComplex z);
+	AComplex& operator=(TComplex z);
+
+	AComplex& operator+=(AComplex z);
+	AComplex& operator+=(TComplex z);
+	AComplex& operator-=(AComplex z);
+	AComplex& operator-=(TComplex z);
+
+	static  const double getAngle(const AComplex z);
+	static const double getLength(const AComplex z);
 };
 
+AComplex& operator+(AComplex z1, AComplex z2);
+AComplex& operator+(AComplex z1, TComplex z2);
+AComplex& operator-(AComplex z1, AComplex z2);
+AComplex& operator-(AComplex z1, TComplex z2);
